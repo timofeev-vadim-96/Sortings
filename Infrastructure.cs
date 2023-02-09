3ed7d6adf62b042de1169d391f7e21c73fbecf04
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-09 22:03:06 +0300

    infrastructure was added

---
+++ Infrastructure.cs
@@ -0,0 +1,16 @@
+using static System.Console;
+using static System.String;
+public static class Infrastructure
+{
+    public static int [] CreateArray (int size, int min = 0, int max = 10)
+    {
+        return Enumerable.Range(1,size)
+                        .Select (item => Random.Shared.Next(min,max))
+                        .ToArray;
+    }
+
+    public static void Show (int [] inputArray)
+    {
+        WriteLine(Join(", ", inputArray));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// Сортировка выбором
 using static Sorting;
-using static System.Console;
-int[] basicArray = { 10, 4, 24, 65, 1, 23, 45, 63, 45, 1, 3, 15 };
+using static Infrastructure;
+int[] basicArray = CreateArray(10);
+Show(basicArray);
 SortSelection(basicArray);
-WriteLine(String.Join(", ", basicArray));
+Show(basicArray);
+
