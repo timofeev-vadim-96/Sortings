5afecc61f235e7abb9adcd4ce12490b94ec8f745
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-09 21:36:53 +0300

    Sorting was added

---
+++ Sorting.cs
@@ -0,0 +1,20 @@
+public static class Sorting
+{
+    public static void SortSelection(int[] inputArray)
+    {
+        for (int i = 0; i < inputArray.Length - 1; i++)
+        {
+            int pos = i;
+            for (int l = i + 1; l < inputArray.Length; l++)
+            {
+                if (inputArray[l] < inputArray[pos])
+                {
+                    pos = l; //запоминаем позицию минимального элемента в массиве
+                }
+            }
+            int temp = inputArray[i]; // запоминаем число на позиции i
+            inputArray[i] = inputArray[pos]; // минимальный элемент на i позицию
+            inputArray[pos] = temp; // окончательно меняем местами числа
+        }
+    }
+}
