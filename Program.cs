79e6a383a026607aff70e1b92c36297511bc7986
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-02-09 21:36:33 +0300

    Добавил файл сортинг с методом для сортировки выбором и фишечки для вызова этого метода и сокращения кода для вывода текста в консоль

--- Program.cs
+++ Program.cs
@@ -1,21 +1,6 @@
 ﻿// Сортировка выбором
+using static Sorting;
+using static System.Console;
 int[] basicArray = { 10, 4, 24, 65, 1, 23, 45, 63, 45, 1, 3, 15 };
-void SortSelection(int[] inputArray)
-{
-    for (int i = 0; i < basicArray.Length - 1; i++)
-    {
-        int pos = i;
-        for (int l = i + 1; l < basicArray.Length; l++)
-        {
-            if (basicArray[l] < basicArray[pos])
-            {
-                pos = l;
-            }
-        }
-        int temp = basicArray[i];
-        basicArray[i] = basicArray[pos];
-        basicArray[pos] = temp;
-    }
-}
 SortSelection(basicArray);
-System.Console.WriteLine(String.Join(", ", basicArray));
+WriteLine(String.Join(", ", basicArray));
